fad7f650964a091ba81836602c43ec640ea8f67a
wangfs <wangfs@jurassic.com.cn>
2023-10-08 16:04:35 +0800

    feat：局部地区雨滴效果

--- src/components/MyMap.vue
+++ src/components/MyMap.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="map"></div>
+  <div id="map">
+  </div>
+  <button class="switch_rain" @click="closeRain">关闭局部雨滴</button>
+    <button class="switch_webgl" @click="closeRain">webgl渲染的雨滴</button>
 </template>
 
 <script setup>
@@ -19,14 +22,15 @@ import Text from 'ol/style/Text.js';
 import GeoJSON from 'ol/format/GeoJSON.js';
 import Control from 'ol/control/Control.js';
 import { Circle, Fill, Stroke, Style } from 'ol/style.js';
+import Icon from 'ol/style/Icon.js';
+import rainUrl from '@/assets/rain.png'
 import { onMounted } from 'vue'
-let map, extent, sliderControl, fullScreenControl, markLayer, markerSource
+let map, extent, sliderControl, fullScreenControl, markLayer, markerSource, rainLayer, rainSource, orScope
 const orZoom = 12
 const orCenter = [114.30, 30.50]
 const blackMap= new TileLayer({
   source: new XYZ({
-    // url: 'http://wprd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}'
-    url: 'https://mt1.google.com/vt/lyrs=t&x={x}&y={y}&z={z}'
+    url: 'https://webst01.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}'
   }),
   visible:false
 })
@@ -50,17 +54,6 @@ const initMap = () => {
       projection: 'EPSG:4326'
     })
   })
-  markerSource = new VectorSource()
-  markLayer = new VectorLayer({
-    source: markerSource,
-    minZoom: 11,
-    maxZoom: 13,
-    id: 'markLayer'
-  })
-  map.addLayer(markLayer)
-}
-// 初始化extent控件
-const initExtent = () => {
   //获取再zoomlevel等于12时候的分辨率（即每个像素点的长度）
   const resolution = map.getView().getResolutionForZoom(orZoom)
   //获取当前视图的像素大小
@@ -72,10 +65,29 @@ const initExtent = () => {
   const maxX = orCenter[0] + width / 2;
   const maxY = orCenter[1] + height / 2;
   // 构建范围对象
-  const extentScope = [minX, minY, maxX, maxY];
+  orScope = [minX, minY, maxX, maxY];
+  markerSource = new VectorSource()
+  rainSource = new VectorSource()
+  markLayer = new VectorLayer({
+    source: markerSource,
+    minZoom: 11,
+    maxZoom: 13,
+    id: 'markLayer'
+  })
+  rainLayer = new VectorLayer({
+    source: rainSource,
+    opacity: 0.5,
+    className: 'rain_mask',
+    visible:false
+  })
+  map.addLayer(rainLayer)
+  map.addLayer(markLayer)
+}
+// 初始化extent控件
+const initExtent = () => {
   extent = new ZoomToExtent({
     label: '回武汉',
-    extent: extentScope
+    extent: orScope
   })
   map && map.addControl(extent)
 }
@@ -89,6 +101,7 @@ const initSliderControl = () => {
   sliderControl = new ZoomSlider()
   map && map.addControl(sliderControl)
 }
+// 初始化圆点
 const initCircle = (cor) => {
   const point = new Feature({
     geometry: new Point(cor)
@@ -149,18 +162,19 @@ const loadGeoJson = (data) => {
     })) 
   })
   markerSource.addFeatures(features)
-  window.map=map
 }
+// 点击地图
 const initClickEvent = () => {
   if (!map) return
   map.on('click', event => {
-    const {coordinate, pixel } = event
+    const {coordinate } = event
     initCircle(coordinate)
     map.getView().animate({
       center: coordinate
     })
   })
 }
+// 切换底图
 const initSwitchButton = () => {
   const button = document.createElement('button')
   button.innerHTML='切换底图'
@@ -179,15 +193,72 @@ const initSwitchButton = () => {
   })
   map && map.addControl(switchButton)
 }
+
+const initRainSymbol = (cor) => {
+  const point = new Feature({
+    geometry: new Point(cor)
+  })
+  const style = new Style({
+    image: new Icon({
+      src: rainUrl,
+      width: 5,
+      height: 5
+    }),
+  })
+  point.setStyle(style)
+  return point
+}
+let dis = []
+const points = []
+const cord=[]
+// 初始划下雨
+const initRain = () => {
+  const count = 500
+  for (let i = 0; i < count; i++){
+    let x = (Math.random() * (orScope[2] - orScope[0])) + orScope[0]
+    let y = (Math.random() * (orScope[3] - orScope[1])) + orScope[1]
+    cord.push([x,y])
+    let p = initRainSymbol([x, y])
+    dis.push(y - orScope[1])
+    points.push(p)
+  }
+  rainSource.addFeatures(points)
+  const fea = rainSource.getFeatures()
+  window.ge=fea[0]
+}
+
+
+const time = new Array(500).fill(0)
+// 雨滴下落
+const rainFall = () => {
+  const fea = rainSource.getFeatures()
+  fea.forEach((f, i) => {
+    if (time[i] > dis[i]) {
+      time[i] = 0
+      f.getGeometry().setCoordinates(cord[i])
+    } else {
+      time[i] += 0.002
+      f.getGeometry().translate(0, -0.002)
+    }
+  })
+  requestAnimationFrame(rainFall)
+}
+// 关闭雨图层
+const closeRain = () => {
+  console.log(1234)
+  const v = rainLayer.getVisible()
+  rainLayer.setVisible(!v)
+}
 onMounted(() => {
   initMap()
   initExtent()
   initSliderControl()
   initFullScreen()
-  initCircle(orCenter)
   initClickEvent()
   loadGeoJson(geojsonData)
   initSwitchButton()
+  initRain()
+  rainFall()
 })
 </script>
 
@@ -213,5 +284,18 @@ onMounted(() => {
   position: absolute;
   right: 5.5em;
   top:0.5em
+}
+.rain_mask{
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.switch_rain{
+  position: absolute;
+  right: 11.5em;
+  top:1.5em
+}
+.switch_webgl{
+  position: absolute;
+  right: 20.5em;
+  top:1.5em
 }
 </style>
